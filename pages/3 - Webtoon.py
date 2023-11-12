23fede2ad7317e8b7785cf19e0ef2bcff4821cc1
jylee-ds <139599902+jylee-ds@users.noreply.github.com>
2023-11-12 09:17:09 +0900

    Update 3 - Webtoon.py

--- pages/3 - Webtoon.py
+++ pages/3 - Webtoon.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 
-image = Image.open(r'/Users/jylee/streamlit.venv/bin/HackerTon_1111/pages/KakaoTalk_Photo_2023-11-12-03-44-15.png')
+image = Image.open(r'pages/KakaoTalk_Photo_2023-11-12-03-44-15.png')
 
 st.header('GLC 졸업요건 웹툰')
 st.image(image, caption = '졸업 요건 웹툰')
